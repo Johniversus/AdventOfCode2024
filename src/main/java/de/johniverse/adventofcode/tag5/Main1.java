906295813fac763a4d0a388cb343886440d3170f
Justin Ivers <Justin.Ivers@adesso-health.de>
2024-12-05 17:05:27 +0100

    Tag5

---
+++ src/main/java/de/johniverse/adventofcode/tag5/Main1.java
@@ -0,0 +1,70 @@
+package de.johniverse.adventofcode.tag5;
+
+import de.johniverse.adventofcode.share.InputLoader;
+import io.vavr.Tuple2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main1 {
+    public static void main(String[] args) {
+        List<String> input = InputLoader.getInput("adventofcode/tag5/input.txt");
+
+        List<Tuple2<Integer, Integer>> regeln = createRegeln(input);
+        List<List<Integer>> rawUpdates = createRawUpdates(input);
+
+        List<List<Integer>> richtigeUpdates = new ArrayList<>();
+
+        for (List<Integer> rawUpdate : rawUpdates){
+            List<Tuple2<Integer, Integer>> anwendbareRegeln = regeln.stream()
+                    .filter(regel -> rawUpdate.contains(regel._1()) && rawUpdate.contains(regel._2()))
+                    .toList();
+
+            if (updateInvalide(rawUpdate, anwendbareRegeln))
+                richtigeUpdates.add(rawUpdate);
+        }
+
+        // Berechnung Ergebnis:
+        int ergebnis = richtigeUpdates.stream()
+                .mapToInt(update -> update.get(update.size() / 2))
+                .sum();
+
+        System.out.println("Ergebnis: " + ergebnis);
+
+    }
+
+    private static boolean updateInvalide(List<Integer> rawUpdate, List<Tuple2<Integer, Integer>> anwendbareRegeln) {
+        return anwendbareRegeln.stream()
+                .noneMatch(anwendbareRegel -> {
+                    int ersteRegel = anwendbareRegel._1();
+                    int zweiteRegel = anwendbareRegel._2();
+
+                    int stelleErsteZahl = rawUpdate.indexOf(ersteRegel);
+                    int stelleZweiteZahl = rawUpdate.indexOf(zweiteRegel);
+
+                    return stelleErsteZahl > stelleZweiteZahl;
+                });
+
+    }
+
+    private static List<Tuple2<Integer, Integer>> createRegeln(List<String> input) {
+        return input.stream()
+                .takeWhile(zeile -> !zeile.isEmpty())
+                .map(zeile -> {
+                    String[] tempRegeln = zeile.split("\\|");
+                    return new Tuple2<>(Integer.parseInt(tempRegeln[0]), Integer.parseInt(tempRegeln[1]));
+                })
+                .collect(Collectors.toList());
+    }
+
+    private static List<List<Integer>> createRawUpdates(List<String> input) {
+        return input.stream()
+                .dropWhile(zeile -> !zeile.isEmpty())
+                .skip(1)
+                .map(zeile -> Arrays.stream(zeile.split(","))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList())
+                )
+                .collect(Collectors.toList());
+    }
+}
